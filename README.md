76ab28e975f95e4b4cd6c3fe3a126f38a5fe0b8f
Justice Nyaga <justiceevansmwema@gmail.com>
2024-03-15 14:09:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# My Terminal Configurations
+
+This repository contains my terminal configuration setups for xfce4 and gnome terminal.
+
+## Clone the repo
+
+```bash
+git clone https://github.com/justicenyaga/terminal-config.git
+```
+
+## xfce4 Terminal
+
+### Install xfce4 Terminal
+
+Run the following command to install xfce4 terminal
+
+```bash
+sudo apt install xfce4-terminal
+```
+
+### Set xfce4 Terminal as the default terminal emulator
+
+This step is optional. If you want to set xfce4 terminal as the default terminal emulator, run the following commands
+
+```bash
+sudo update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper
+gsettings set org.gnome.desktop.default-applications.terminal exec 'xfce4-terminal'
+```
+
+### Overwrite Open Terminal shotcut (Ubuntu)
+
+Go to `Settings -> Keyboard -> Keyboard Shortcuts (View and Customize Shortcuts) -> Custom Shortcuts` and create a new shortcut.
+
+```
+Name: Launch XFCE Terminal
+Command: exo-open --launch TerminalEmulator
+Shortcut: Ctrl + Alt + T
+```
+
+This will overwrite the default shortcut that launches gnome-terminal.
+
+### Configuration Script
+
+Run the setup script to configure xfce4 terminal
+
+```bash
+cd terminal-config && sh setup.sh
+```
+
+## Gnome Terminal
+
+To apply the profile configuration file on a given profile, run the following command:
+
+```bash
+dconf load /org/gnome/terminal/legacy/profiles:/:profile_id < profile.dconf
+```
+
+Replace "profile_id" with the specific ID of the profile you wish to overwrite. You can use the tab key for convenient autocompletion of the profile_id.
+
+`Ensure you have CodeNewRoman Nerd Font Mono installed. If it's not already installed, you can find it in the attached copy on this repository and install it from there..`
---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+font="CodeNewRomanNerdFontMono-Regular.otf"
+fonts_dir="$HOME/.local/share/fonts"
+terminal_dir="$HOME/.config/xfce4/terminal"
+
+# Check if fonts directory exists
+if [ ! -d "$fonts_dir" ]; then
+    mkdir -p "$fonts_dir"
+fi
+
+# Copy the font to the fonts directory
+if [ ! -f "$fonts_dir/$font" ]; then
+    cp "$(pwd)/$font" "$fonts_dir"
+fi
+
+# Check if terminal configuration directory exists
+if [ ! -d "$terminal_dir" ]; then
+    mkdir -p "$terminal_dir"
+fi
+
+# Copy accels.scm file
+if [ -f "$terminal_dir/accels.scm" ]; then
+    rm "$terminal_dir/accels.scm"
+fi
+cp "$(pwd)/accels.scm" "$terminal_dir"
+
+# Generate the terminalrc config file
+background_image="$(pwd)/background-1.jpg"
+cat > "$terminal_dir/terminalrc" <<EOF
+[Configuration]
+FontName=CodeNewRoman Nerd Font Mono 14
+MiscAlwaysShowTabs=FALSE
+MiscBell=FALSE
+MiscBellUrgent=FALSE
+MiscBordersDefault=TRUE
+MiscCursorBlinks=TRUE
+MiscCursorShape=TERMINAL_CURSOR_SHAPE_BLOCK
+MiscDefaultGeometry=80x24
+MiscInheritGeometry=FALSE
+MiscMenubarDefault=TRUE
+MiscMouseAutohide=FALSE
+MiscMouseWheelZoom=TRUE
+MiscToolbarDefault=FALSE
+MiscConfirmClose=TRUE
+MiscCycleTabs=TRUE
+MiscTabCloseButtons=TRUE
+MiscTabCloseMiddleClick=TRUE
+MiscTabPosition=GTK_POS_TOP
+MiscHighlightUrls=TRUE
+MiscMiddleClickOpensUri=FALSE
+MiscCopyOnSelect=FALSE
+MiscShowRelaunchDialog=TRUE
+MiscRewrapOnResize=TRUE
+MiscUseShiftArrowsToScroll=FALSE
+MiscSlimTabs=FALSE
+MiscNewTabAdjacent=FALSE
+MiscSearchDialogOpacity=100
+MiscShowUnsafePasteDialog=FALSE
+MiscRightClickAction=TERMINAL_RIGHT_CLICK_ACTION_CONTEXT_MENU
+ColorForeground=#b9f6d110b695
+ColorBackground=#0c0c1c1b2525
+ColorPalette=rgb(21,37,53);rgb(255,60,60);rgb(73,255,109);rgb(255,188,81);rgb(0,175,255);rgb(142,68,173);rgb(22,160,133);rgb(189,195,199);rgb(86,97,109);rgb(255,60,76);rgb(147,255,145);rgb(255,255,85);rgb(91,215,255);rgb(155,89,182);rgb(32,92,87);rgb(255,255,255)
+BackgroundMode=TERMINAL_BACKGROUND_IMAGE
+BackgroundDarkness=0.850000
+BackgroundImageFile=$background_image
+BackgroundImageShading=0.780000
+BackgroundImageStyle=TERMINAL_BACKGROUND_STYLE_FILLED
+EOF
+
